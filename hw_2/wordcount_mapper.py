d5f42c135938f50e944274ffd214ebcb499210e1
davemo88 <davidkasofsky@gmail.com>
2015-09-22 12:03:12 -0400

    hw_2 done

--- hw_2/wordcount_mapper.py
+++ hw_2/wordcount_mapper.py
@@ -3,9 +3,13 @@
 import sys
 import re
 
+## pass the patterns to be matched as positional arguments to this script
 patterns = sys.argv[1:]
 
 for line in sys.stdin:
+## note input and patterns are both sent to lower case
+## for case insensitive matching
     val = line.strip().lower()
     for p in patterns:
+## count the number of times each pattern appears in the input
         print "%s\t%s" % (p, len(re.findall(p.lower(), val)))
--- hw_2/wordcount_reducer.py
+++ hw_2/wordcount_reducer.py
@@ -1,13 +1,20 @@
 #!/usr/bin/env python
 import sys
 
+## to handle key changes
 last_key, count = None, 0
+
 for line in sys.stdin:
     (key, val) = line.strip().split('\t')
+
+## when we change keys, emit the k-v pair
+## for last key and start counting for new key
     if last_key and last_key!= key:
         print '%s\t%s' % (last_key, count)
         (last_key, count) = (key, int(val))
+## in this case the key hasn't changed yet
     else:
         (last_key, count) = (key, (count + int(val)))
+## emit the k-v pair for the last key in the input
 if last_key:
     print '%s\t%s' % (last_key, count)
